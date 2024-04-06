33b59dd754722812da4dc0192c2e63b2eded7d70
jlawrence2910 <166222267+jlawrence2910@users.noreply.github.com>
2024-04-06 15:26:24 +0200

    update button to run in forever loop

--- main.py
+++ main.py
@@ -2,37 +2,60 @@ def on_button_event_p13_down():
     joystickbit.Vibration_Motor(100)
     basic.show_string("F")
     radio.send_string('forward')
-joystickbit.on_button_event(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.DOWN, on_button_event_p13_down)
+#joystickbit.on_button_event(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.DOWN, on_button_event_p13_down)
+
 
 def on_button_event_p14_down():
     joystickbit.Vibration_Motor(100)
     basic.show_string("B")
-    radio.send_message('backward')
-joystickbit.on_button_event(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.DOWN, on_button_event_p14_down)
+    radio.send_string('backward')
+#joystickbit.on_button_event(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.DOWN, on_button_event_p14_down)
 
-#def on_button_event_p12_down():
-#    joystickbit.Vibration_Motor(100)
-#    basic.show_string("L")
-#    radio.send_string("left")
+
+def on_button_event_p15_down():
+    joystickbit.Vibration_Motor(100)
+    basic.show_string("R")
+    radio.send_string('right')
+#joystickbit.on_button_event(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.DOWN, on_button_event_p15_down)
+
+
+def on_button_event_p12_down():
+    joystickbit.Vibration_Motor(100)
+    basic.show_string("L")
+    radio.send_string('left')
 #joystickbit.on_button_event(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.DOWN, on_button_event_p12_down)
 
-#def on_button_event_p15_down():
-#    joystickbit.Vibration_Motor(100)
-#    basic.show_string("R")
-#    #radio.send_message('right')
-#joystickbit.on_button_event(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.DOWN, on_button_event_p15_down)
 
 def on_button_event_B_down():
     joystickbit.Vibration_Motor(100)
     basic.show_string("S")
     radio.send_string('stop')
 
+
 joystickbit.Vibration_Motor(100)
 joystickbit.init_joystick_bit()
 
+
 radio.set_group(1)
+
 
 def on_forever():
     if input.button_is_pressed(Button.B):
         on_button_event_B_down()
+
+    if joystickbit.get_button(joystickbit.JoystickBitPin.P12):
+        music.play_tone(330, music.beat(BeatFraction.HALF))
+        on_button_event_p12_down()
+
+    if joystickbit.get_button(joystickbit.JoystickBitPin.P13):
+        music.play_tone(349, music.beat(BeatFraction.HALF))
+        on_button_event_p13_down()
+
+    if joystickbit.get_button(joystickbit.JoystickBitPin.P14):
+        music.play_tone(392, music.beat(BeatFraction.HALF))
+        on_button_event_p14_down()
+
+    if joystickbit.get_button(joystickbit.JoystickBitPin.P15):
+        music.play_tone(440, music.beat(BeatFraction.HALF))
+        on_button_event_p15_down()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,35 +1,63 @@
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function on_button_event_p13_down() {
+function on_button_event_p13_down() {
     joystickbit.Vibration_Motor(100)
     basic.showString("F")
     radio.sendString("forward")
-})
-// radio.send_message('backward')
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function on_button_event_p14_down() {
+}
+
+// joystickbit.on_button_event(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.DOWN, on_button_event_p13_down)
+function on_button_event_p14_down() {
     joystickbit.Vibration_Motor(100)
     basic.showString("B")
-})
-// def on_button_event_p12_down():
-//     joystickbit.Vibration_Motor(100)
-//     basic.show_string("L")
-//     radio.send_string("left")
-// joystickbit.on_button_event(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.DOWN, on_button_event_p12_down)
-// def on_button_event_p15_down():
-//     joystickbit.Vibration_Motor(100)
-//     basic.show_string("R")
-//     #radio.send_message('right')
+    radio.sendString("backward")
+}
+
+// joystickbit.on_button_event(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.DOWN, on_button_event_p14_down)
+function on_button_event_p15_down() {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("R")
+    radio.sendString("right")
+}
+
 // joystickbit.on_button_event(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.DOWN, on_button_event_p15_down)
+function on_button_event_p12_down() {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("L")
+    radio.sendString("left")
+}
+
+// joystickbit.on_button_event(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.DOWN, on_button_event_p12_down)
 function on_button_event_B_down() {
     joystickbit.Vibration_Motor(100)
     basic.showString("S")
+    radio.sendString("stop")
 }
 
-// radio.send_message('stop')
 joystickbit.Vibration_Motor(100)
 joystickbit.initJoystickBit()
 radio.setGroup(1)
 basic.forever(function on_forever() {
     if (input.buttonIsPressed(Button.B)) {
         on_button_event_B_down()
+    }
+    
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        music.playTone(330, music.beat(BeatFraction.Half))
+        on_button_event_p12_down()
+    }
+    
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        music.playTone(349, music.beat(BeatFraction.Half))
+        on_button_event_p13_down()
+    }
+    
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        music.playTone(392, music.beat(BeatFraction.Half))
+        on_button_event_p14_down()
+    }
+    
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
+        music.playTone(440, music.beat(BeatFraction.Half))
+        on_button_event_p15_down()
     }
     
 })
